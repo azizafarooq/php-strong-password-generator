01a3dabe52e51940894db03a1098c06530cd8210
azizafarooq <azizafarooq2000@gmail.com>
2024-02-28 16:57:35 +0100

    feat: function inizialize

--- index.php
+++ index.php
@@ -1,5 +1,14 @@
 <?php
 $password_length = $_GET['length'];
+$upper_case = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+$lower_case = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+$numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+$symbols = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '_', '+', '=', '{', '}', '[', ']', '|', ':', ';', '"', "'", '<', '>', ',', '.', '?', '/'];
+
+function generate_password(){
+    //genera password randomica
+    return $random_password;
+}
 ?>
 
 <!DOCTYPE html>
